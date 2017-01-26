3107a9285b2d244bafeaa37e373a6a3b37e99e2c
AmrAbdelhameed <amrabdelhameedfcis123@gmail.com>
2017-01-27 00:06:27 +0200

    as

--- app/src/main/java/com/example/amr/firebaseregistrationloginexam/ProfileActivity.java
+++ app/src/main/java/com/example/amr/firebaseregistrationloginexam/ProfileActivity.java
@@ -32,6 +32,7 @@ public class ProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
         firebaseAuth = FirebaseAuth.getInstance();
+        //user
         user = firebaseAuth.getCurrentUser();
 
         mFirebaseInstance = FirebaseDatabase.getInstance();
